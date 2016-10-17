5f4ff970b897cefe4775546a07e65e58656d282a
Willian Oizumi <willian.oizumi@ifpr.edu.br>
2016-10-17 17:50:12 -0200

    initial commit

---
+++ src/main/java/ifpr/tads/esi/Calculadora.java
@@ -0,0 +1,20 @@
+package ifpr.tads.esi;
+
+public class Calculadora {
+	
+	public static double soma(double n1, double n2) {
+		return n1 + n2;
+	}
+	
+	public static double subtracao(double n1, double n2) {
+		return n1 - n2;
+	}
+	
+	public static double multiplicacao(double n1, double n2) {
+		return n1 * n2;
+	}
+	
+	public static double divisao(double n1, double n2) {
+		return n1 / n2;
+	}
+}
---
+++ src/test/java/ifpr/tads/esi/TesteCalculadora.java
@@ -0,0 +1,28 @@
+package ifpr.tads.esi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TesteCalculadora {
+	
+	@Test
+	public void testeSoma() {
+		Assert.assertEquals(5, Calculadora.soma(2., 3.), 0.001);
+	}
+	
+	@Test
+	public void testeSubtracao() {
+		Assert.assertEquals(1, Calculadora.subtracao(3., 2.), 0.001);
+	}
+	
+	@Test
+	public void testeMultiplicacao() {
+		Assert.assertEquals(10., Calculadora.multiplicacao(2., 5.), 0.001);
+	}
+	
+	@Test
+	public void testeDivisao() {
+		Assert.assertEquals(2., Calculadora.divisao(10., 5.), 0.001);
+	}
+
+}
